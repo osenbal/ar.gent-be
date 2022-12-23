1bcf3e0addad7e8eb8a0e4e4ec4b0b2e875f7826
Iqbal Maulana <100358555+osenbal2@users.noreply.github.com>
2022-12-23 09:04:08 +0000

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -50,7 +50,6 @@ class App {
    *
    **/
   private initialMiddlewares() {
-    this.app.use("etag");
     this.app.use(morgan(LOG_FORMAT, { stream }));
     this.app.use(cookieParser());
     this.app.use(bodyParser.urlencoded({ extended: true }));
